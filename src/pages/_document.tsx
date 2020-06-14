c8e84bbd4b0fec9b45a2a320ed2f2cad089fc247
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-06-14 08:42:20 -0300

    analytics on head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-interface GoogleTagsData {
-  __html: string;
-}
-
 class MyDocument extends Document {
-  setGoogleTags(): GoogleTagsData {
-    return {
-      __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-169305250-1');
-      `,
-    };
-  }
-
   render(): React.ReactElement {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-169305250-1"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-169305250-1');
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
-          <NextScript>
-            <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=UA-169305250-1"
-            />
-
-            <script dangerouslySetInnerHTML={this.setGoogleTags()} />
-          </NextScript>
+          <NextScript />
         </body>
       </Html>
     );
